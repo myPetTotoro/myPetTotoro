542fba97120a89df3ab77ee7296bb37b47da74c7
BrentonPedler <brenton_pedler@hotmail.com>
2021-11-09 20:43:56 -0500

    added the user input and capitalize method

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ guessGhibliApp.randomChar = (userChoice) => {
             const guessWho = computerChoice[0].people;
 
             let randomChar = guessWho[Math.floor(Math.random() * guessWho.length)];
+            guessGhibliApp.getCharacterDetails(randomChar)
             console.log(`this is the random character:`, randomChar);
 
         })
@@ -60,13 +61,38 @@ guessGhibliApp.getCharacterDetails = (charURL) => {
             const eyeColor = data.eye_color;
             const hairColor = data.hair_color;
             console.log(name, age, gender, eyeColor, hairColor);
-
+            guessGhibliApp.userInput(name);
         }
     )}
+
+guessGhibliApp.userInput = (name) => {
+    const userForm = document.getElementById('userResponse');
+    userForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        const userText = document.getElementById('textField');
+        const userInput = userText.value;
+
+        const capitalizedResponse = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+        
+        console.log(capitalizedResponse);
+        
+        console.log(userInput);
+
+        if (userInput == name) {
+            console.log('cool man');
+        } else {
+            console.log('shiiiiii');
+        }
+    })
+}
+
 
 guessGhibliApp.init = () => {
     // Get Characters and Display in Drop Down
     guessGhibliApp.getCharacters();
+
+    guessGhibliApp.userInput();
 
     // Get User Choice through Event Listener
     const selectValue = document.getElementById('characterType');
